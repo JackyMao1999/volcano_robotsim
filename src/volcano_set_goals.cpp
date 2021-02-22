ffe2f3f0a15e4b7662e29a637dc56cb777ff0d25
JackyMao1999 <48790075+JackyMao1999@users.noreply.github.com>
2021-02-22 09:38:49 +0800

    Update volcano_set_goals.cpp

--- src/volcano_set_goals.cpp
+++ src/volcano_set_goals.cpp
@@ -1,127 +1,99 @@
-#include <signal.h>
-#include <std_msgs/String.h>
-#include <move_base_msgs/MoveBaseAction.h>
-#include <actionlib/client/simple_action_client.h>
+#include "string.h"
+#include "volcano_robotsim/VolcanoGoal.h" 
+#include "geometry_msgs/PoseStamped.h"
 #include "ros/ros.h"
-#include <geometry_msgs/Twist.h> 
-#include <webots_ros/set_float.h>
-#include <webots_ros/set_int.h>
-#include <webots_ros/Int32Stamped.h>
  
 using namespace std;
-
-#define TIME_STEP 32    //时钟
  
 ros::NodeHandle *n;
-
-static int controllerCount;
-static std::vector<std::string> controllerList; 
-
-ros::ServiceClient timeStepClient;          //时钟通讯客户端
-webots_ros::set_int timeStepSrv;            //时钟服务数据
-
-typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
-
-
-/*******************************************************
-* Function name ：controllerNameCallback
-* Description   ：控制器名回调函数，获取当前ROS存在的机器人控制器
-* Parameter     ：
-        @name   控制器名
-* Return        ：无
-**********************************************************/
-// catch names of the controllers availables on ROS network
-void controllerNameCallback(const std_msgs::String::ConstPtr &name) { 
-    controllerCount++; 
-    controllerList.push_back(name->data);//将控制器名加入到列表中
-    ROS_INFO("Controller #%d: %s.", controllerCount, controllerList.back().c_str());
-}
-
-
-/*******************************************************
-* Function name ：quit
-* Description   ：退出函数
-* Parameter     ：
-        @sig   信号
-* Return        ：无
-**********************************************************/
-void quit(int sig) {
-    ROS_INFO("User stopped the '/volcano' node.");
-    timeStepSrv.request.value = 0; 
-    timeStepClient.call(timeStepSrv); 
-    ros::shutdown();
-    exit(0);
-}
-
+ros::Publisher pub_goal;
+void goalCallback(const volcano_robotsim::VolcanoGoal::ConstPtr &value);
 
 int main(int argc, char **argv) {
    
-    std::string controllerName;
     // create a node named 'volcano' on ROS network
-    ros::init(argc, argv, "volcano_set_goal", ros::init_options::AnonymousName);
+    ros::init(argc, argv, "volcano_set_goal");
     n = new ros::NodeHandle;
-
-    signal(SIGINT, quit);
-
-    // subscribe to the topic model_name to get the list of availables controllers
-    ros::Subscriber nameSub = n->subscribe("model_name", 100, controllerNameCallback);
-    while (controllerCount == 0 || controllerCount < nameSub.getNumPublishers()) {
-        ros::spinOnce();
-        ros::spinOnce();
-        ros::spinOnce();
+    ros::Subscriber sub_goal;
+    sub_goal = n->subscribe("/volcano/goal",1,goalCallback);
+    
+    pub_goal = n->advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal",2);
+    ROS_INFO("Started success ");
+    ros::spin();
+}
+void goalCallback(const volcano_robotsim::VolcanoGoal::ConstPtr &value){
+    int isture=0;
+    string goal_name = value->goal_name;
+    geometry_msgs::PoseStamped target_pose;
+    target_pose.header.seq = 1;
+    target_pose.header.frame_id = "map";
+    
+    if (goal_name == "A"||goal_name == "B")
+    {
+        target_pose.pose.position.x = -2.0;
+        target_pose.pose.position.y = 6.29;
+        target_pose.pose.orientation.z = 0.0016;
+        target_pose.pose.orientation.w = 0.9999;
+        isture = 1;
+    }else if(goal_name == "C"||goal_name == "D")
+    {
+        target_pose.pose.position.x = -2.0;
+        target_pose.pose.position.y = 3.7;
+        target_pose.pose.orientation.z = 0.0016;
+        target_pose.pose.orientation.w = 0.9999;
+        isture = 1;
+    }else if (goal_name == "E"||goal_name == "F")
+    {
+        target_pose.pose.position.x = -2.0;
+        target_pose.pose.position.y = 1.21;
+        target_pose.pose.orientation.z = 0.0016;
+        target_pose.pose.orientation.w = 0.9999;
+        isture = 1;
+    }else if (goal_name == "G"||goal_name == "H")
+    {
+        target_pose.pose.position.x = -2.0;
+        target_pose.pose.position.y = -1.21;
+        target_pose.pose.orientation.z = 0.0016;
+        target_pose.pose.orientation.w = 0.9999;
+        isture = 1;
+    }else if (goal_name == "T"||goal_name == "TB"||goal_name == "TD")
+    {
+        target_pose.pose.position.x = -7.0;
+        target_pose.pose.position.y = 5.95;
+        target_pose.pose.orientation.z = 0.9999;
+        target_pose.pose.orientation.w = 0.0028;
+        isture = 1;
+    }else if (goal_name == "TE"||goal_name == "TF"||goal_name == "TG")
+    {
+        target_pose.pose.position.x = -7.0;
+        target_pose.pose.position.y = 3.6;
+        target_pose.pose.orientation.z = 0.9999;
+        target_pose.pose.orientation.w = 0.0028;
+        isture = 1;
+    }else if (goal_name == "TH"||goal_name == "TJ"||goal_name == "TK")
+    {
+        target_pose.pose.position.x = -7.0;
+        target_pose.pose.position.y = 1.11;
+        target_pose.pose.orientation.z = 0.9999;
+        target_pose.pose.orientation.w = 0.0028;
+        isture = 1;
+    }else if (goal_name == "TL"||goal_name == "TM"||goal_name == "TN")
+    {
+        target_pose.pose.position.x = -7.0;
+        target_pose.pose.position.y = -1.19;
+        target_pose.pose.orientation.z = 0.9999;
+        target_pose.pose.orientation.w = 0.0028;
+        isture = 1;
     }
-    ros::spinOnce();
 
-    timeStepClient = n->serviceClient<webots_ros::set_int>("volcano/robot/time_step");
-    timeStepSrv.request.value = TIME_STEP;
-
-    // if there is more than one controller available, it let the user choose
-    if (controllerCount == 1)
-        controllerName = controllerList[0];
-    else {
-        int wantedController = 0;
-        std::cout << "Choose the # of the controller you want to use:\n";
-        std::cin >> wantedController;
-        if (1 <= wantedController && wantedController <= controllerCount)
-        controllerName = controllerList[wantedController - 1];
-        else {
-        ROS_ERROR("Invalid number for controller choice.");
-        return 1;
-        }
+    if (isture)
+    {
+        target_pose.header.stamp = ros::Time::now();
+        pub_goal.publish(target_pose);
+        ROS_INFO("Ready to go to the goal %s",goal_name.c_str());
     }
-    ROS_INFO("Using controller: '%s'", controllerName.c_str());
-    // leave topic once it is not necessary anymore
-    nameSub.shutdown();
-
-    
-    //tell the action client that we want to spin a thread by default
-    MoveBaseClient ac("move_base", true);
-    
-    //wait for the action server to come up
-    while(!ac.waitForServer(ros::Duration(5.0))){
-        ROS_INFO("Waiting for the move_base action server to come up");
-    }
-
-    move_base_msgs::MoveBaseGoal goal;
-
-    //we'll send a goal to the robot to move 1 meter forward
-    goal.target_pose.header.frame_id = "map";
-    goal.target_pose.header.stamp = ros::Time::now();
-
-    goal.target_pose.pose.position.x = 1.0;
-    goal.target_pose.pose.position.y = 1.0;
-    goal.target_pose.pose.position.z = 0.0;
-    goal.target_pose.pose.orientation.w = 1.0;
-    
-    ROS_INFO("Sending goal");
-    ac.sendGoal(goal);
-    
-    ac.waitForResult();
-    
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-        ROS_INFO("Hooray, the base moved 1 meter forward");
     else
-        ROS_INFO("The base failed to move forward 1 meter for some reason");
-
-    return 0;
+    {
+        ROS_ERROR("Can't compare the goal");
+    }
 }
